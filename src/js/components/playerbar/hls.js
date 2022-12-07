f001b50423b38733ce83f441bdcf239a46f82f93
Ibrahim Aftab <ibrahim.aftab@tapmad.com>
2022-12-07 17:28:33 +0500

    hlsjs fixed

--- src/js/components/playerbar/hls.js
+++ src/js/components/playerbar/hls.js
@@ -3,61 +3,57 @@ import events from "../../events";
 export default function (player) {
   const self = player.playerbar;
   const hls = player.hlsjs;
-  hls.on(Hls.Events.LEVEL_SWITCHED, function () {
-    if (
-      hls.currentLevel > 0 ||
-      player.querySelector("#quality-list.active")?.textContent == "Auto"
-    ) {
-      player.querySelector(".quality-auto").textContent = hls.levels.find(
-        (level, i) => i === hls.currentLevel
-      ).height;
-    }
-  });
-  hls.on(Hls.Events.BUFFER_CREATED, function () {
-    if (hls.levels.length > 1) {
-      self.createQualityDropdown(function () {
-        hls.levels.forEach((level, i) => {
+  self.addEventListener("playerbar-initial-ready", function () {
+    hls.on(Hls.Events.LEVEL_SWITCHED, function () {
+      if (
+        hls.currentLevel > 0 ||
+        player.querySelector("#quality-list.active")?.textContent == "Auto"
+      ) {
+        player.querySelector(".quality-auto").textContent = hls.levels.find(
+          (level, i) => i === hls.currentLevel
+        ).height;
+      }
+    });
+    hls.on(Hls.Events.BUFFER_CREATED, function () {
+      if (hls.levels.length > 1) {
+        self.createQualityDropdown(function () {
+          hls.levels.forEach((level, i) => {
+            player
+              .querySelector("dropdown #quality-list")
+              .insertAdjacentHTML(
+                "beforeend",
+                `<button data-id="${i}">${level.height}</button>`
+              );
+          });
           player
             .querySelector("dropdown #quality-list")
             .insertAdjacentHTML(
               "beforeend",
-              `<button data-id="${i}">${level.height}</button>`
+              `<button class="active" data-id="-1">Auto <span class="quality-auto">${hls.levels[0].height}</span></button>`
             );
-        });
-        player
-          .querySelector("dropdown #quality-list")
-          .insertAdjacentHTML(
-            "beforeend",
-            `<button class="active" data-id="-1">Auto <span class="quality-auto">${hls.levels[0].height}</span></button>`
-          );
-        player
-          .querySelectorAll(`#quality-list button:not(.dropdown-back)`)
-          .forEach(function (elm, i) {
-            elm.addEventListener("click", (e) => {
-              const btn = e.target;
-              if (!elm.classList.contains("active")) {
-                const { id } = btn.dataset;
-                player
-                  .querySelector(`#quality-list button.active`)
-                  .classList.remove("active");
-                btn.classList.add("active");
-                hls.currentLevel = Number(id);
-                if (Number(id) > 0) {
-                  player.querySelector(".quality-auto").textContent =
-                    hls.levels.find((level, i) => i === Number(id)).height;
+          player
+            .querySelectorAll(`#quality-list button:not(.dropdown-back)`)
+            .forEach(function (elm, i) {
+              elm.addEventListener("click", (e) => {
+                const btn = e.target;
+                if (!elm.classList.contains("active")) {
+                  const { id } = btn.dataset;
+                  player
+                    .querySelector(`#quality-list button.active`)
+                    .classList.remove("active");
+                  btn.classList.add("active");
+                  hls.currentLevel = Number(id);
+                  if (Number(id) > 0) {
+                    player.querySelector(".quality-auto").textContent =
+                      hls.levels.find((level, i) => i === Number(id)).height;
+                  }
                 }
-              }
+              });
             });
-          });
-      });
-    }
+        });
+      }
+    });
   });
-  let initiate = false;
-  hls.on(Hls.Events.LEVEL_UPDATED, function (_, event) {
-    if (!initiate) {
-      initiate = true;
-      const live = event.details.live;
-      self.initiate(!live);
-    }
-  });
+  console.log(player.live)
+  self.initiate(!player.live);
 }
--- src/js/functions/hls.js
+++ src/js/functions/hls.js
@@ -7,20 +7,27 @@ export default async function (player, url) {
   if (Hls.isSupported()) {
     const hls = new Hls();
     hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-      triggerEvent(events.dynamicHlsJs, player);
-      triggerEvent(events.initiated, player);
+      triggerEvent(events.initiate, player);
     });
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
     player.hlsjs = hls;
-    hls.on(Hls.Events.LEVEL_UPDATED, function () {
-      triggerEvent(events.loaded, player);
+    let initiate = false;
+    hls.on(Hls.Events.LEVEL_UPDATED, function (event) {
+      if (!initiate) {
+        initiate = true;
+        triggerEvent(events.loaded, player);
+        player.live = event.details.live;
+      }
     });
      hls.on(Hls.Events.LEVEL_SWITCHING, function () {
        triggerEvent(events.loading, player);
      });
     hls.on(Hls.Events.LEVEL_SWITCHED, function () {
       triggerEvent(events.loaded, player);
+    });
+    player.addEventListener(events.videoReady, function() {
+      triggerEvent(events.dynamicHlsJs, player)
     });
   } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
     video.src = videoSrc;
